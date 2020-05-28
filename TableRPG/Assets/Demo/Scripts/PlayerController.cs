95715e4a196ae76c2798c39a1d3a5cc1bfea813f
João Ricardo dos Santos <ricardoelric@gmail.com>
2020-05-28 00:44:14 -0300

    imp: setting wall shadows and player controller
    - removing old tiles sprites and setting more simples
    - adding Cinemachine in the project
    - Creating player movement controller
    - base test with lights in scene

---
+++ TableRPG/Assets/Demo/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float velocity = 10f;
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Move(horizontal, vertical);
+    }
+
+    private void Move(float horizontal, float vertical)
+    {
+        if (horizontal == 0 && vertical == 0) return;
+
+        Vector3 position = new Vector3(horizontal, vertical, 0f);
+
+        position *= (Time.deltaTime * velocity);
+        position.z = transform.position.z;
+
+        transform.position += position;
+    }
+
+
+}
