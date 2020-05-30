fbcbea74d8f50cd1d5b50490835ef00185f848fd
João Ricardo dos Santos <ricardoelric@gmail.com>
2020-05-30 02:03:07 -0300

    imp: Creating the light controller to disable ligths blockeds by a wall
    understanting base limits of ligths 2d

---
+++ TableRPG/Assets/Demo/Scripts/LightController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class LightController : MonoBehaviour
+{
+    [SerializeField]
+    private Light2D globalLight;
+
+    private List<Light2D> lights;
+
+    private PlayerController playerRef;
+    private void Awake()
+    {
+        PlayerController.PlayerStart += SavePlayerReference;
+        this.lights = new List<Light2D>(FindObjectsOfType<Light2D>());
+        this.lights.Remove(this.globalLight);
+    }
+
+    private void OnDestroy()
+    {
+        PlayerController.PlayerStart -= SavePlayerReference;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (this.playerRef == null) return;
+
+        foreach (var item in this.lights)
+        {
+            if (playerRef.LightReference == item) continue;
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(this.playerRef.transform.position, item.transform.position);
+            Gizmos.color = Color.white;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (this.playerRef == null) return;
+        if (!this.playerRef.Moving) return;
+        DisableRayCastedLights();
+    }
+
+    private void SavePlayerReference(PlayerController player)
+    {
+        this.playerRef = player;
+        DisableRayCastedLights();
+    }
+
+    private void DisableRayCastedLights()
+    {
+        foreach (var item in this.lights)
+        {
+            if (playerRef.LightReference == item) continue;
+
+            int layerIndex = LayerMask.NameToLayer("Walls");            
+
+            var direction = (item.transform.position - this.playerRef.transform.position);
+
+            RaycastHit2D value = Physics2D.Raycast(this.playerRef.transform.position, direction, Mathf.Infinity, 1 << layerIndex);
+
+            var active = false;
+
+            if (value.collider == null)
+            {
+                active = true;
+            }
+
+            item.gameObject.SetActive(active);
+
+        }
+    }
+}
--- TableRPG/Assets/Demo/Scripts/PlayerController.cs
+++ TableRPG/Assets/Demo/Scripts/PlayerController.cs
@@ -1,22 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class PlayerController : MonoBehaviour
 {
+    public delegate void OnPlayerStart(PlayerController player);
+    public static OnPlayerStart PlayerStart;
+
     [SerializeField]
     private float velocity = 10f;
+
+    [SerializeField]
+    private Light2D lightReference;
+
+    private bool moving;
+
+    private void Start()
+    {
+        if (PlayerStart != null) PlayerStart(this);
+    }
+
     private void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Move(horizontal, vertical);
+        this.moving = Move(horizontal, vertical);
     }
 
-    private void Move(float horizontal, float vertical)
+    public bool Moving
     {
-        if (horizontal == 0 && vertical == 0) return;
+        get { return this.moving; }
+    }
+
+    public Light2D LightReference
+    {
+        get { return this.lightReference; }
+    }
+
+    private bool Move(float horizontal, float vertical)
+    {
+        if (horizontal == 0 && vertical == 0) return false;
 
         Vector3 position = new Vector3(horizontal, vertical, 0f);
 
@@ -24,7 +49,6 @@ public class PlayerController : MonoBehaviour
         position.z = transform.position.z;
 
         transform.position += position;
+        return true;
     }
-
-
 }
